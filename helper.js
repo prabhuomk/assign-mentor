a9f088c2a14488863b6a0833269b41bd5ae5515e
prabhuomk <prabhuomk@gmail.com>
2021-10-11 10:13:26 +0530

    add signup login

--- helper.js
+++ helper.js
@@ -1,3 +1,44 @@
+import mongodb from "mongodb";
+
+//signup login
+
+export async function insertUser(client, user) {
+    const result = await client.db("mentor_student").collection("user").insertOne(user);
+    console.log("successfully pass inserted", result);
+    return result;
+}
+
+export async function getUser(client, filter) {
+    const result = await client.db("mentor_student").collection("user").findOne(filter);
+    console.log("successfully matched", result);
+    return result;
+}
+
+export async function updateUser(client, _id,password) {
+    const result = await client.db("mentor_student").collection("user").updateOne({ _id:new mongodb.ObjectId(_id) },{$set:{password:password}});
+    console.log("successfully new password updated", result);
+    return result;
+}
+
+export async function inserttoken(client, user) {
+    const result = await client.db("mentor_student").collection("tokens").insertOne(user);
+    console.log("successfully pass inserted", result);
+    return result;
+}
+
+export async function gettoken(client, filter) {
+    const result = await client.db("mentor_student").collection("tokens").findOne(filter);
+    console.log("successfully matched", result);
+    return result;
+}
+
+
+export async function deletetoken(client,tokenid){
+    const deletetokens= await client.db("mentor_student").collection("tokens").deleteOne({tokenid:new mongodb.ObjectId(tokenid)});
+    console.log("successfully token is deleted",deletetokens);
+    return deletetokens;
+}
+
 // for mentor collection
 
 export async function insertMentor(client,Mentor){
--- routes/user.js
+++ routes/user.js
@@ -10,12 +10,116 @@ import {
   getOneStudent,
   deleteMyMentor,
   updateStudentsAfterMentorDelete,
-  deleteMyStudent
+  deleteMyStudent,insertUser,getUser,updateUser,inserttoken,gettoken,deletetoken
 } from "../helper.js";
 import { createConnection } from "../index.js";
 import express, { response } from "express";
 
 const router = express.Router();
+
+
+//signup and login part
+
+router
+.route("/signup")
+.post(async (request,response)=>{
+    const { email_id,firstname,lastname,password }= request.body;
+    
+    
+
+    const client=await createConnection();
+    const hashedPassword=await genPassword(password);
+
+    const pass=await insertUser(client,{email_id:email_id,firstname:firstname,lastname:lastname,password:hashedPassword})
+    console.log(hashedPassword,pass );
+    response.send(pass);
+    
+});
+
+router
+.route("/login")
+.post(async (request,response)=>{
+    const { email_id,password }= request.body;
+    const client=await createConnection();
+    const user=await getUser(client,{email_id:email_id});
+    if(!user){
+        response.send({message:"user not exist ,please sign up"})
+    }else{
+    console.log(user._id);
+    
+    const inDbStoredPassword=user.password;
+    const isMatch= await bcrypt.compare(password,inDbStoredPassword);
+    if(isMatch){
+        const token=jwt.sign({id:user._id},process.env.KEY)
+    
+        response.send({message:"successfully login",token:token,email_id:email_id});
+    }
+    else{
+        response.send({message:"invalid login"});
+
+    } 
+} 
+    
+});
+
+router
+.route("/forgetpassword")
+.post(async (request,response)=>{
+    const { email_id }= request.body;
+    const client=await createConnection();
+    const user=await getUser(client,{email_id});
+    if(!user){
+        response.send({message:"user not exist"})
+    }else{
+
+        const token=jwt.sign({id:user._id},process.env.REKEY);
+        const expiryDate= Date.now()+3600000;
+        const store= await inserttoken(client,{tokenid:user._id,token:token,expiryDate:expiryDate});
+        const link = `${process.env.BASE_URL}/password-reset/${user._id}/${token}`;
+       
+      const mail=  await sendEmail(user.email_id, "Password reset", link);
+    response.send({message:"link has been send to your email for password change"});
+
+    } 
+} 
+    
+);
+
+router
+.route("/resetpassword/:id/:token")
+.post(async (request,response)=>{
+    const { password }= request.body;
+    const id=request.params.id;
+    const token=request.params.token;
+    const client=await createConnection();
+    const tokens=await gettoken(client,{token:token});
+    if(!tokens){
+        response.send({message:"invalid token"})
+    }else{
+        if( Date.now()< tokens.expiryDate ){
+        const hashedPassword=await genPassword(password);
+        const updateuserpassword = await updateUser(client,id,hashedPassword);
+        const deletetokens= await deletetoken(client,id);
+        response.send({message:"password updated and token got deleted"})
+
+    } 
+    else{
+        response.send({message:"link got expired"})
+    }
+
+} 
+  
+});
+
+
+
+async function genPassword(password){
+    
+    const salt=await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+    return hashedPassword;
+}
+
 
 //create mentor
 
